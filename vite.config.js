362ffa3a21531eca0f74165476f44301a8f24f3d
Adrian <TU_MAIL>
2026-08-24 20:23:31 -0300

    Asegurar registro automático del Service Worker en la configuración de la PWA

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
       includeAssets: ['favicon.svg', 'icons/*.png'],
       manifest: {
         name: 'Gestión APSOL',
